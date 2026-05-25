a474406dda169029b1d03892e06a22c18fc64231
LASTYYYYYYYYYYY <patatostas2@mail.ru>
2026-05-25 19:27:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,54 @@
 import OBR from "https://unpkg.com/@owlbear-rodeo/sdk@latest/dist/index.mjs";
 
-let lastSelection = [];
-
-async function gatherTokens() {
-
-  if (!lastSelection.length) {
-    console.log("No saved selection");
-    return;
-  }
-
-  const items = await OBR.scene.items.getItems(
-    item => lastSelection.includes(item.id)
-  );
-
-  if (!items.length) {
-    console.log("No items");
-    return;
-  }
-
-  const center =
-    items[Math.floor(Math.random() * items.length)];
-
-  const baseX = center.position.x;
-  const baseY = center.position.y;
-
-  await OBR.scene.items.updateItems(
-    lastSelection,
-    drafts => {
-
-      let offset = 0;
-
-      for (const item of drafts) {
-
-        item.position.x = baseX;
-        item.position.y = baseY - offset;
-
-        offset += 20;
-      }
-
-    }
-  );
-
-  console.log("Moved!");
-
-}
+let savedSelection = [];
 
 OBR.onReady(async () => {
 
-  // следим за выделением
+  // постоянно сохраняем выделение
   OBR.player.onChange(player => {
-
-    lastSelection = player.selection;
-
-    console.log("Saved selection:", lastSelection);
-
+    savedSelection = player.selection;
   });
 
-  // запуск при открытии popup
-  gatherTokens();
+  const button = document.getElementById("gather");
+
+  button.addEventListener("click", async () => {
+
+    if (!savedSelection.length) {
+      console.log("No selection");
+      return;
+    }
+
+    const items = await OBR.scene.items.getItems(
+      item => savedSelection.includes(item.id)
+    );
+
+    if (!items.length) {
+      return;
+    }
+
+    const center =
+      items[Math.floor(Math.random() * items.length)];
+
+    const baseX = center.position.x;
+    const baseY = center.position.y;
+
+    await OBR.scene.items.updateItems(
+      savedSelection,
+      drafts => {
+
+        let offset = 0;
+
+        for (const item of drafts) {
+
+          item.position.x = baseX;
+          item.position.y = baseY - offset;
+
+          offset += 20;
+        }
+
+      }
+    );
+
+  });
 
 });
