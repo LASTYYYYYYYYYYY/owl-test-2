b545330d6a7cc6fa62fe84f5561ed8502eb67a9b
LASTYYYYYYYYYYY <patatostas2@mail.ru>
2026-05-25 19:36:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,42 @@
 import OBR from "https://unpkg.com/@owlbear-rodeo/sdk@latest/dist/index.mjs";
 
-let savedSelection = [];
-
 OBR.onReady(() => {
-
-  // сохраняем выделение
-  OBR.player.onChange((player) => {
-    savedSelection = player.selection || [];
-    console.log("Selection:", savedSelection);
-  });
 
   const button = document.getElementById("gather");
 
   button.addEventListener("click", async () => {
 
-    if (savedSelection.length === 0) {
-      console.log("No selection");
+    const selection = await OBR.player.getSelection();
+
+    console.log(selection);
+
+    if (selection.length === 0) {
+      console.log("nothing selected");
       return;
     }
 
-    // получаем выбранные items
-    const items = await OBR.scene.items.getItems(
-      item => savedSelection.includes(item.id)
-    );
+    const items = await OBR.scene.items.getItems(selection);
+
+    console.log(items);
 
     if (items.length === 0) {
-      console.log("No items found");
+      console.log("no items");
       return;
     }
 
-    // случайный токен = база
     const center =
       items[Math.floor(Math.random() * items.length)];
 
-    const baseX = center.position.x;
-    const baseY = center.position.y;
+    let offset = 0;
 
-    // ВАЖНО:
-    // updateItems(items, ...)
-    // а не updateItems(ids, ...)
     await OBR.scene.items.updateItems(
       items,
       (drafts) => {
 
-        let offset = 0;
-
         for (const item of drafts) {
 
-          item.position.x = baseX;
-          item.position.y = baseY - offset;
+          item.position.x = center.position.x;
+          item.position.y = center.position.y - offset;
 
           offset += 20;
         }
@@ -56,7 +44,7 @@ OBR.onReady(() => {
       }
     );
 
-    console.log("Moved!");
+    console.log("DONE");
 
   });
 
